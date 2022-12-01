14dcea7c6baee07ebba12f920e6412499018ec98
Administrator <Purplejamkiev@gmail.com>
2022-12-01 21:04:58 +0200

    minor fixes

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -6,35 +6,29 @@ const moment = require('moment')
 
 const getAllJobs = async (req, res) => {
   const {search, status, jobType, sort} = req.query
+  //search jobs with userId only
   const queryObject = {
     createdBy: req.user.userId
   }
+  //add search in queryObj
+  if (search && search !== 'all') queryObject.position = search
+  if (status && status !== 'all') queryObject.status = status
+  if (jobType && jobType !== 'all') queryObject.jobType = jobType
   
-  if (search && search !== '') {
-    queryObject.position = {
-      $regex: search,
-      $options: 'i'
-    }
-  }
-  if (jobType && jobType !== 'all') {queryObject.jobType = jobType}
-  if (status && status !== 'all') {queryObject.status = status}
-
+  //make queryObj promise
   let result = Job.find(queryObject)
-  if (sort === 'latest') {result = result.sort('-createdAt')}
-  if (sort === 'oldest') {result = result.sort('createdAt')}
-  if (sort === 'a-z') {result = result.sort('position')}
-  if (sort === 'z-a') {result = result.sort('-position')}
-
-  const page = req.query.page || 1
-  const limit = req.query.limit || 8
+ 
+  //add sorting logic
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 4
   const skip = (page - 1) * limit
 
   const totalJobs = await Job.countDocuments(queryObject)
   const numOfPages = Math.ceil(totalJobs / limit)
 
   result = result.skip(skip).limit(limit)
-  const jobs = await result
-
+  const jobs = await Job.find(result)
+  
   res.status(StatusCodes.OK).json({jobs, numOfPages, totalJobs})
 }
 
